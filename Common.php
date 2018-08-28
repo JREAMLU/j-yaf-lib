dc33ed5efba3ae4100b1f39a93f376d0fbad12f6
jream <ms.08.lu@gmail.com>
2018-08-28 11:58:32 +0800

    feat(ip2region):

--- Common.php
+++ Common.php
@@ -3,7 +3,18 @@
 namespace App\Lib;
 
 class Common {
-    public function dealImg() {
-        return "dealimg";
+
+    public function ip2region($ip) {
+        $info = Ip2region::btreeSearchString($ip);
+        $region = explode('|', $info['region']);
+
+        return [
+            'city_id' => $info['city_id'],
+            'country' => $region[0] == '0' ? '-' : $region[0],
+            'area' => $region[1] == '0' ? '-' : $region[1],
+            'province' => $region[2] == '0' ? '-' : $region[2],
+            'city' => $region[3] == '0' ? '-' : $region[3],
+            'isp' => $region[4] == '0' ? '-' : $region[4],
+        ];
     }
 }

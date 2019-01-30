64fc1ce7feaf1f5592c5961ca262dbebffa98e76
jream <ms.08.lu@gmail.com>
2019-01-30 11:03:19 +0800

    feat(crypto): gen key

--- Crypto.php
+++ Crypto.php
@@ -75,4 +75,13 @@ class Crypto {
 
         return substr($decrypted_data, 0, -1 * $padding);
     }
+
+    public function genKey($length = 8) {
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $str = '';
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $chars[mt_rand(0, strlen($chars) - 1)];
+        }
+        return $str;
+    }
 }

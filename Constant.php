5bf10c8b8adc0e3248d0c665277bad69dbf1c4aa
jream <ms.08.lu@gmail.com>
2019-01-30 14:51:41 +0800

    feat(login): constant

--- Constant.php
+++ Constant.php
@@ -7,6 +7,7 @@ class Constant {
     const SUCCESS_CODE = 0;
     const SYSTEM_ERROR_CODE = 1000;
     const PARAMS_ERROR_CODE = 1001;
+    const NOT_LOGIN = 1002;
 
     const YAF_ERR_NOTFOUND_MODULE = '找不到指定的模块';
     const YAF_ERR_NOTFOUND_CONTROLLER = '找不到指定的Controller';
@@ -17,5 +18,6 @@ class Constant {
         self::SUCCESS_CODE => '成功',
         self::SYSTEM_ERROR_CODE => '系统错误',
         self::PARAMS_ERROR_CODE => '参数错误',
+        self::NOT_LOGIN => '登录态失效',
     ];
 }
